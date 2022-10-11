081094dedbfe3ce70e28c9b0597fa71be8a021de
anto112 <m07158031@o365.mcut.edu.tw>
2022-10-11 11:02:48 +0800

    first upload

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -1,8 +1,12 @@
 # import necessary library you used here
-from src.views.ui_template import Ui_MainWindow
+from src.views.image_processing import Ui_MainWindow
 from src.models.moilutils import mutils
 from src.models.model import Model
-
+import pyqtgraph as pq
+from pyqtgraph.Qt import QtCore, QtGui, uic
+import cv2
+# import matplotlib.pylab as plt
+from matplotlib import pyplot as plt
 
 class Controller(Ui_MainWindow):
     def __init__(self, parent):
@@ -17,3 +21,40 @@ class Controller(Ui_MainWindow):
         super().__init__()
         self.setupUi(parent)
         self.model = Model()
+        # self.horizontalSlider.setMinimum(0)
+        # self.horizontalSlider.setMaximum(255)
+        # self.horizontalSlider.setValue(1)
+
+        self.pushButton.clicked.connect(self.open_file)
+        self.pushButton_2.clicked.connect(self.filtering)
+        self.horizontalSlider.valueChanged.connect(self.slide)
+
+    def open_file(self):
+        file = mutils.select_file(title="select image", file_filter="*.jpg, *.png")
+        self.img = mutils.read_image(file)
+        mutils.show_image_to_label(self.label, self.img, 400)
+        # image_filter = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        # mutils.show_image_to_label(self.label_2, image_filter, 400)
+        # mutils.show_image_to_label(self.label_5, hist2, 400)
+        hist = cv2.calcHist([self.img], [0], None, [256], [0, 256])
+        plt.plot(hist)
+        plt.show()
+    def filtering(self):
+        try:
+            gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+            mutils.show_image_to_label(self.label_2, gray, 400)
+            # hist = cv2.calcHist([self.label_2], [0], None, [256], [0, 256])
+            # plt.plot(hist)
+            # mutils.show_image_to_label(self.label_5, plt.show(), 400)
+            self.plotwi
+        except:
+            print("Input the image first")
+
+    def slide(self, value):
+        self.label_3.setText(str(value))
+        value_text = self.label_3.text()
+        self.value = int(value_text)
+        gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        self.image = cv2.blur(gray, (self.value, self.value))
+        # cv2.imshow("test", new_image)
+        mutils.show_image_to_label(self.label_2, self.image, 400)
--- src/views/ui_template.py
+++ src/views/ui_template.py
@@ -14,35 +14,38 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(465, 368)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        font = QtGui.QFont()
-        font.setFamily("Sans Serif")
-        font.setPointSize(16)
-        self.label.setFont(font)
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName("label")
-        self.verticalLayout.addWidget(self.label)
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab_3 = QtWidgets.QWidget()
+        self.tab_3.setObjectName("tab_3")
+        self.tabWidget.addTab(self.tab_3, "")
+        self.tab_4 = QtWidgets.QWidget()
+        self.tab_4.setObjectName("tab_4")
+        self.tabWidget.addTab(self.tab_4, "")
+        self.verticalLayout.addWidget(self.tabWidget)
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout.addWidget(self.pushButton_2)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Hello, Youre ready to create an application using MoilApps Template !!!"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("MainWindow", "Tab 1"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("MainWindow", "Tab 2"))
+        self.pushButton_2.setText(_translate("MainWindow", "PushButton"))
 
 
 if __name__ == "__main__":
